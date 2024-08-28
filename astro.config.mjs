466610a74dd65c741b63aa510cc809f56be66fe1
dww510 <dww510@gmail.com>
2024-08-28 02:33:18 -0700

    removed rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,7 @@ export default defineConfig({
   site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
+  sitemap: true,
   image: {
     service: squooshImageService(),
   },
